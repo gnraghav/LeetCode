afddf9b1a030a999235a85b2a456f463cb4ddac0
Raghavendra Naik <raghunaik207@gmail.com>
2024-08-04 20:37:02 +0530

    Time: 222 ms (29.92%), Space: 76.9 MB (7.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,16 +5,14 @@ public:
             return 0;
         }
         
-        set<int> s; // to keep track of unique element
         map<int, bool> mp; //  for faster finding the element
         for (auto i: nums) {
-            s.insert(i);
             mp[i] = true;
         }
         int cons = 0;
         int maxCons = 0;
-        for (auto i: s) {
-             if (mp.count(i) && mp.count(i+1)) {
+        for (auto i: mp) {
+             if (mp.count(i.first+1)) {
                  cons += 1;
              } else {
                  maxCons = max(maxCons, cons+1);
