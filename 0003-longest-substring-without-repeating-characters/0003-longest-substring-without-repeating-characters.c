8cd0f2cc5bc2b75ab76e3de6e6acbabe5c38e949
Raghavendra Naik <raghunaik207@gmail.com>
2024-08-06 19:55:42 +0530

    Time: 4 ms (62.58%), Space: 9.8 MB (14.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -4,13 +4,13 @@ int lengthOfLongestSubstring(char* s) {
         a[i] = -1;  // Initialize all elements to -1
     }
     int res = 0;
-    int start = -1;  // Index starts with -1 to handle the first character correctly
-    for (int i = 0; s[i] != '\0'; i++) {
+    int start = -1; // index starts with zero.
+    for (int i=0;s[i]!='\0';i++) {
         if (a[s[i]] > start) {
             start = a[s[i]];
         }
         a[s[i]] = i;
-        res = (res > i - start ? res : i - start);
-    }
+        res = (res > i-start ? res : i-start);
+    }   
     return res;
 }
