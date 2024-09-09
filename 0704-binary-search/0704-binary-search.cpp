09db35d70f921b16348de66fd2d1c8462454da28
Raghavendra Naik <raghunaik207@gmail.com>
2024-09-09 21:08:22 +0530

    Time: 18 ms (96.29%), Space: 30.2 MB (24.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
+int divide(int dividend, int divisor) {
+    if (divisor == 0) {
+        printf("Error: Division by zero\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int quotient = 0;
+    int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+    dividend = abs(dividend);
+    divisor = abs(divisor);
+
+    while (dividend >= divisor) {
+        dividend -= divisor;
+        quotient++;
+    }
+
+    return sign * quotient;
+}
+
     int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size()-1;
         
         while(low<=high) {
-            int mid = (low+high)/(1<<1);
+            int mid = divide((low+high), (1<<1));
             if (nums[mid] == target) {
                 return mid;
             }
